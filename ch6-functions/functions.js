5591ec1ae46a4088c6d3221503c352c7654614e3
gpeppas <gpeppas@ajpsoft.gr>
2026-09-12 12:48:30 +0300

    Add examples for optional, default, and rest parameters in functions.js

--- ch6-functions/functions.js
+++ ch6-functions/functions.js
@@ -15,3 +15,69 @@ console.log(add(5, 3))
 const multiply = (a,b) => a * b;
 
 console.log(multiply(4, 6));
+
+// Optional Params
+function createButton(text, color) {
+    color = color || "blue"; 
+    console.log(text, color);
+}
+createButton(`Hello`);
+createButton(`Hello`, '');
+
+// Default params -- E56
+function createButton2(text, color = "blue") {
+    console.log(text, color);
+}
+createButton2(`Hello`);
+createButton2(`Hello`, '');
+
+// Rest Params
+// max(1, 2, 3, 4, 5)
+function max(...numbers) {              //rest
+    // for (const num of numbers) {
+    //     //find max
+    return Math.max(...numbers);        //spread
+
+}
+
+console.log(max(1, 2, 8, 3, 23, 17));
+
+function calculateTotalPrice(discount, ...prices) {
+    const totalPrice = prices.reduce((sum, price) => sum + price, 0);
+    return totalPrice - discount
+}
+
+console.log(calculateTotalPrice(10, 20, 30, 40));
+
+
+// Sanity Checks στα input params των συναρτήσεων
+// έχουν νόημα μόνο αν public APIs, κλπ external data
+const user = {
+    id: 1,
+    firstname: `Alice`,
+    lastname: `W.`,
+    hobbies: [`reading`]
+}
+
+
+//legacy 
+function getFirstHobby(user) {
+    if (!(user && user.hobbies && Array.isArray(user.hobbies) && user.hobbies.length > 0)) {
+        return
+    }
+    return user.hobbies[0]        
+    }
+
+    //Modern way
+    function getFirstHobby2(user) {
+        return user?.hobbies?.[0] ?? `no hobbies`; //nullish coalescing
+    }
+
+    
+// for arrays
+function gradeSum(grades) {
+    if (!Array.isArray(grades) || grades.length === 0) {
+        return 
+    }
+    // for ....
+} 
